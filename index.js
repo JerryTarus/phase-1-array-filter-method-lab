2ba0b1f7843719771bbc8c1a8f54aa9d38d9af3a
Jerry Tarus <jerrytarus@gmail.com>
2023-10-01 16:04:32 +0300

    Flter Method Lab done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+const drivers = ['Jerry', 'Moffat', 'Anisa', 'Yahya', 'Derrick', 'Izzie']
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+console.log(findMatching())
+
+function fuzzyMatch(drivers,name){
+    return drivers.filter(driver => driver[0] === name[0])
+}
+
+function matchName(drivers,name){
+  
+    const matches= drivers.filter(driver => driver.name === name);
+
+    return matches
+}
